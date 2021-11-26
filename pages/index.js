67848bb10557034a63b20efd43e983a56da2c9b2
satyam kushwaha <satyam21@navgurukul.org>
2021-11-26 21:53:42 +0530

    adding global background

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,9 @@ import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
 
+
+
+
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
   return {
